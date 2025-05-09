29d87d0a36705a6d9818a5e2c021131ed2a55026
beye1935 <ruby.nasrun01@gmail.com>
2025-05-09 18:17:15 +0700

    membuat daftar pemain barca

--- utsrevo/src/App.vue
+++ utsrevo/src/App.vue
@@ -2,6 +2,29 @@
 </script>
 
 <template>
+  <div class="app">
+    <img src="https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg" alt="FC Barcelona Logo" class="logo" />
+
+    <h1 class="title">Daftar Pemain Barca</h1>
+    <div class="form">
+      <input v-model="newTask" @keyup.enter="addTask" placeholder="Tambahkan kegiatan" />
+      <button @click="addTask">Tambah</button>
+    </div>
+
+    <div class="filter">
+      <label>
+        <input type="checkbox" v-model="showUnfinishedOnly" /> Tampilkan Pemain Yang Belum Latihan
+      </label>
+    </div>
+
+    <ul>
+      <li v-for="(task, index) in filteredTasks" :key="index">
+        <input type="checkbox" v-model="task.done" />
+        <span :class="{ done: task.done }">{{ task.text }}</span>
+        <button @click="removeTask(index)">Batal</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
